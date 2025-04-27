d1476ab648a0433d24c6d9f142e75eb7376c4998
radutodosan <radutodosan@gmail.com>
2025-04-28 01:43:12 +0300

    added get all mechanics functionality and modified access token expiration time

--- src/main/java/com/radutodosan/mechanics/controllers/MechanicValidationController.java
+++ src/main/java/com/radutodosan/mechanics/controllers/MechanicValidationController.java
@@ -1,7 +1,10 @@
 package com.radutodosan.mechanics.controllers;
 
 import com.radutodosan.mechanics.dtos.ApiResponseDTO;
+import com.radutodosan.mechanics.dtos.MechanicDetailsDTO;
+import com.radutodosan.mechanics.entities.Mechanic;
 import com.radutodosan.mechanics.services.MechanicService;
+import com.radutodosan.mechanics.repositories.MechanicRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +12,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/mechanics")
 @RequiredArgsConstructor
 public class MechanicValidationController {
 
     private final MechanicService mechanicService;
+    private final MechanicRepository mechanicRepository;
 
     @GetMapping("/exists/{username}")
     public ResponseEntity<?> mechanicExists(@PathVariable String username) {
@@ -26,6 +32,21 @@ public class MechanicValidationController {
                 exists
         );
 
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllMechanics() {
+        List<Mechanic> mechanics = mechanicRepository.findAll();
+        List<MechanicDetailsDTO> mechanicDetails = mechanics.stream()
+                .map(mechanic -> new MechanicDetailsDTO(mechanic.getUsername(), mechanic.getEmail()))
+                .toList();
+
+        ApiResponseDTO<List<MechanicDetailsDTO>> response = new ApiResponseDTO<>(
+                true,
+                "Mechanics retrieved successfully",
+                mechanicDetails
+        );
         return ResponseEntity.ok(response);
     }
 }
